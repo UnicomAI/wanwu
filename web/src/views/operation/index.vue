06f5469e36e0c1f9e7b2d8cbdaf7106655821023
dysiliky <dysiliky@163.com>
2026-07-20 10:16:14 +0800

    chore(web): 运营管理里去掉统计分析

--- web/src/views/operation/index.vue
+++ web/src/views/operation/index.vue
@@ -14,51 +14,31 @@
       style="margin-bottom: -20px"
       v-if="checkPerm(operationPerm)"
     >
-      <div
-        :class="['tab', { active: tabActive === 1 }]"
-        @click="tabClick(1)"
-        v-if="checkPerm(statisticsPerm)"
-      >
-        {{ $t('statistics.title') }}
-      </div>
       <!-- 渠道配置无单独权限，有运营管理权限则有渠道配置权限 -->
-      <div :class="['tab', { active: tabActive === 2 }]" @click="tabClick(2)">
+      <div :class="['tab', { active: tabActive === 1 }]" @click="tabClick(1)">
         {{ $t('channel.title') }}
       </div>
     </div>
 
-    <div v-if="tabActive === 1" style="margin: 30px 20px 0 20px">
-      <Statistics />
-    </div>
-    <div v-if="tabActive === 2" style="margin: 0 20px">
+    <div v-if="tabActive === 1" style="margin: 0 20px">
       <Channel />
     </div>
   </div>
 </template>
 
 <script>
-import Statistics from '@/views/permission/statistics';
-import Oauth from '@/views/permission/oauth';
 import Channel from './channel';
 import { checkPerm, PERMS } from '@/router/permission';
 
 export default {
   name: 'Operation',
-  components: { Statistics, Oauth, Channel },
+  components: { Channel },
   data() {
     return {
       radio: '',
       tabActive: 1,
       operationPerm: PERMS.OPERATION,
-      statisticsPerm: PERMS.STATISTIC,
     };
-  },
-  created() {
-    if (checkPerm(this.statisticsPerm)) {
-      this.tabActive = 1;
-    } else {
-      this.tabActive = 2;
-    }
   },
   methods: {
     checkPerm,
