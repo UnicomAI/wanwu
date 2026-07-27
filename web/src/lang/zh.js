25023bc0fa6fd5cc16f4f4a735e17ff00e18ab48
liudb <17190016002@163.com>
2026-07-27 16:28:22 +0800

    feat(web): 上传组件支持多选

--- web/src/lang/zh.js
+++ web/src/lang/zh.js
@@ -155,6 +155,8 @@ export default {
       uploadLimitTip: '最多上传{limit}个文件，支持图片、文档混合上传',
       continueAddFile: '可继续添加文件',
       clickBlankContinueUpload: '点击空白处继续上传',
+      uploadSummary:
+        '本次共上传{total}个文件，其中成功{success}个，失败{failed}个',
     },
     datePicker: {
       placeholder: '选择日期时间',
